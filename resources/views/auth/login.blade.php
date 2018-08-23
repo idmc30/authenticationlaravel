9d0f097aa71de63a67871cce82ae07548894edf7
idmc <id.morales10n@gmail.com>
2018-08-22 21:25:30 -0500

    authentication personalizada

---
+++ resources/views/auth/login.blade.php
@@ -0,0 +1,18 @@
+@extends('layouts.app')
+
+@section('content')
+
+	<div class="row">
+		<div class="container">
+		  <h2>Acceso a la Aplicacion</h2>
+		  <div class="panel panel-default">
+		           <form action="">
+						<div class="form-group">
+							<label for="email"></label>
+							<input type="email" class="form-control" name="email" placeholder="Ingresa tu email">
+						</div>
+					</form>
+		  </div>
+		</div>
+	</div>
+@endsection
---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+	<meta charset="UTF-8">
+	<title>Document</title>
+	<link rel="stylesheet"  href="/css/app.css">
+</head>
+<body>
+	@yield('content')
+	
+</body>
+</html>
