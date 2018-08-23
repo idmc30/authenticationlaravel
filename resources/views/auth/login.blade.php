09dc1df1a489745f59d7c59e9b21d45ffe8aa7cd
idmc <id.morales10n@gmail.com>
2018-08-22 23:40:23 -0500

    desing login

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -3,16 +3,41 @@
 @section('content')
 
 	<div class="row">
+
 		<div class="container">
-		  <h2>Acceso a la Aplicacion</h2>
+		 {{--    <h2>Acceso a la Aplicacion</h2> --}}
 		  <div class="panel panel-default">
-		           <form action="">
+		  	<br>
+		  	<div class="col-md-4" style="display: block;margin-left: auto;margin-right: auto;">
+		  		<div class="card">
+		  			
+		  			  <h5 class="card-header">Acceso a la Aplicación</h5>
+				  <div class="card-body">
+				   <form action="">
 						<div class="form-group">
-							<label for="email"></label>
+							<label for="email">Email</label>
 							<input type="email" class="form-control" name="email" placeholder="Ingresa tu email">
 						</div>
+
+						<div class="form-group">
+							<label for="password">Contraseña</label>
+							<input type="password" class="form-control" name="password" placeholder="Ingresa tu password">
+						</div>
+						<input class="btn btn-primary" type="button" name="btnlogin" value="Ingresar">
 					</form>
+				  </div>
+				</div>
+		  		 
+		  		
+
+		  	</div>
+		           
 		  </div>
+             
+
+
 		</div>
+	</div>
+
 	</div>
 @endsection
